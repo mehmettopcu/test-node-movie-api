04a1a293bd546ba295ec51766cb5200e4d2c2170
mehmettopcu <mehmettopcu@outlook.com.tr>
2020-07-18 13:24:04 +0300

    env file conf

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
   res.json({ error: { message: err.message, status: err.code } });
 });
 
-const port = 8080;
+const port = 80;
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
